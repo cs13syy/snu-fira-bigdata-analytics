ef0f93918e63bb1ed1c02f6b28f23d1145859d5f
Mia Yang <cs13syy@gmail.com>
2018-07-02 22:27:33 +0900

    Update day8-homework(1).py

--- bigdata-analysis-using-python/day8-homework(1).py
+++ bigdata-analysis-using-python/day8-homework(1).py
@@ -1,20 +1,41 @@
-# lab 4-15 (try/exception practice - 2)
+# Assignment Number...: 5
+# Student Name........: 양서윤
+# File Name...........: hw5_양서윤
+# Program Description.: 간단한 if-else 문과 for, while 문을 활용하는 법을 익힌다.
 
-while True:
-    try:
-        number = int(input("임의의 양의 정수를 입력하세요: "))
-        if number <= 1:
-            raise ValueError
-        elif number == 2:
-            print('이 숫자는 소수입니다.')
-        for i in range(2, number):
-            if type(number) == int and number > 0 and number % i == 0:
-                print('{} x {} = {}'.format(i, int(number/i), number),
-                      '이 숫자는 소수가 아닙니다.', sep='\n')
-                break
-        for j in range(2, number):
-            if number == 2 or number % i != 0:
-                print('이 숫자는 소수입니다.')
-                break
-    except:
-        print('ValueError: 1보다 큰 양의 정수를 입력하세요.')
+
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+c = int(input('Enter c: '))
+
+if a > b and a > c:
+    print(b + c)
+elif b > a and b > c:
+    print(a + c)
+else:
+    print(a + b)
+
+
+city = input('Enter the name of the city: ')
+if city == 'Seoul':
+    print('The size of {} is {}'.format(city, 605))
+elif city == 'New York':
+    print('The size of {} is {}'.format(city, 789))
+elif city == 'Beijing':
+    print('The size of {} is {}'.format(city, 16808))
+else:
+    print('Unknown')
+
+
+import math
+for i in range(1, 10):
+    n = math.factorial(i)
+    print(n)
+
+
+import math
+num = 0
+while num < 10:
+    n = math.factorial(num)
+    print(n)
+    num += 1
