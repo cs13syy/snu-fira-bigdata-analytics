7153d676e6d69fd054b0b3ab16685b9084077ca1
Mia Yang <cs13syy@gmail.com>
2018-09-24 00:00:51 +0900

    Update lab.R

--- advanced-bigdata-analysis/chapter3-target-marketing/lab.R
+++ advanced-bigdata-analysis/chapter3-target-marketing/lab.R
@@ -2,8 +2,33 @@
 #### ch3. target marketing
 ####
 
+# load data
 setwd('C:/Users/yuniv/OneDrive/바탕 화면/SNU FIRA/8_고급빅데이터분석(김용대교수님)/chapter-3')
 buytest <- read.table('buytest.txt', header=TRUE)
 dim(buytest)
-dim(buytest[complete.cases(buytest),])
-buytest(!complete.cases(buytest),)
+names(buytest)
+
+# data preprocessing
+buytest[buytest$sex == "", 'SEX'] = NA
+levels(buytest$sex)[1] = NA
+buytest[buytest$ORGSRC == "", 'ORGSRC'] = NA
+levels(buytest$ORGSRC)[1] = NA
+colnames(buytest)
+buydata <- buytest[,-c(1,10,19:26)]
+buydata <- buydata[complete.cases(buydata),]
+buydata <- model.matrix(~., buydata)[,-1]
+head(buydata)
+head(buytest)
+
+# train and test set
+set.seed(123)
+train_ind <- sample(1:nrow(buydata), size=floor((nrow(buydata))*0.7), replace=F)
+train <- as.data.frame(buydata[train_ind,])
+test <- as.data.frame(buydata[,1])
+train.X <- buydata[train_ind,-1]
+train.Y <- buydata[train_ind,1]
+test.X <- buydata[-train_ind,-1]
+test.Y <- buydata[-train_ind,1]
+dim(train.X)
+dim(test.X)
+
