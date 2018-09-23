b1bb64d19cb097d21e2306ee1a9af254a1c44b1a
Mia Yang <cs13syy@gmail.com>
2018-09-24 00:21:43 +0900

    Update lab.R

--- advanced-bigdata-analysis/chapter3-target-marketing/lab.R
+++ advanced-bigdata-analysis/chapter3-target-marketing/lab.R
@@ -32,3 +32,37 @@ test.Y <- buydata[-train_ind,1]
 dim(train.X)
 dim(test.X)
 
+# distribution of RESPOND
+par(mfrow=c(1,2))
+barplot(table(train.Y), main='the distribution of RESPOND in train set')
+barplot(table(test.Y), main='the distribution of RESPOND in test set')
+
+# logistic regression
+logit <- glm(RESPOND~., as.data.frame(buytest), family='binomial')
+# summary(logit)
+
+# logistic regression (train)
+logit.train <- glm(RESPOND~., data=train, family='binomial')
+prob <- predict(logit.train, test, type='response')
+prob[1:10]
+
+# cutoff
+cutoff <- 0.5
+ifelse(prob[1:10]>cutoff,1,0)
+cutoff < 1/6
+ifelse(prob[1:10]>cutoff,1,0)
+
+# classification function
+classification = function(model, newdata, cutoff){
+  prob = predict(model, newdata, 'response')
+  ifelse(prob>cutoff,1,0)
+}
+
+# classification table
+table(test$RESPOND, classification(logit.train, test, 0.5))
+
+crosstable <- function(model, newdata, cutoff){
+  table(test$RESPOND, classification(model, newdata, cutoff))
+}
+
+crosstable(logit.train, test, 1/4)
